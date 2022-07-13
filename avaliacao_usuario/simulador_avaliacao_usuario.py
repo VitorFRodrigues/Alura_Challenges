3fbc8742fb13aa3bcb7d371f13ecfe16f9e6ff8a
Vitor Fernando de Souza Rodrigues <vitorfbaiano@gmail.com>
2022-07-13 20:37:26 -0300

    avaliacao usuario

--- avaliacao_usuario/simulador_avaliacao_usuario.py
+++ avaliacao_usuario/simulador_avaliacao_usuario.py
@@ -5,9 +5,18 @@ import pandas as pd
 #Cor de fundo do listbox
 st.markdown('<style>div[role="listbox"] ul{background-color: #eee1f79e};</style>', unsafe_allow_html=True)
 
-def avaliar(dict_respostas):
+def avaliar(traduzido_value):
     modelo = load('objetos/modelo.joblib')
+    descricao_colunas = load('objetos/descricao_colunas.joblib')
+    
+    respostas = []
+    for coluna in list(descricao_colunas.keys())[2:]:
+        respostas.append(traduzido_value[coluna])
+    
+    df_novo_usuario = pd.DataFrame(data=[respostas], columns=list(descricao_colunas.keys())[2:])
+    user = modelo.predict(df_novo_usuario)[0]
 
+    return user
 
 st.image('img/Alura_Voz.png')
 st.write('## Simulador avaliação de usuário')
@@ -57,11 +66,11 @@ with assinatura:
 with contrato:
     col5_form, col6_form = st.beta_columns(2)
     dict_respostas['tenure'] = col5_form.slider(label="Mês de contrato do cliente",
-                                                min_value=1, max_value=75, step=1)
+                                                min_value=1, max_value=70, step=1)
     dict_respostas['Contract'] = col6_form.selectbox("Tipo de contrato",
                                                 ('anual', 'mensal', 'bianual'))
     dict_respostas['PaperlessBilling'] = col6_form.radio("Tipo de fatura (online/papel)",
-                                                ('Sim', 'Não'))
+                                                ('Online', 'Papel'))
     dict_respostas['PaymentMethod'] = col6_form.selectbox("Forma de pagamento",
                                                 ('Fatura correio', 'Debito automático', 'Cartão de crédito (automática)', 'Transferência bancária (automática)'))
     dict_respostas['Charges.Monthly'] = col5_form.slider(label="Total de todos os serviços do cliente por mês",
@@ -69,33 +78,20 @@ with contrato:
     dict_respostas['Charges.Total'] = col5_form.slider(label="Total gasto pelo cliente",
                                                 min_value=0.00, max_value=10000.00, step=500.00)
 
+tradutor = load('objetos/tradutor.joblib')
+transformador = load('objetos/transformador.joblib')
+traduzido = {}
+traduzido_value = {}
+for keys,values in dict_respostas.items():
+    if values in tradutor.keys():
+        traduzido[keys] = tradutor[values]
+        traduzido_value[keys] = transformador[traduzido[keys]]
+    elif isinstance(values, (float, int)):
+        traduzido[keys] = values
+        traduzido_value[keys] = values
+
 if st.button('Churn Rate'):
-    if avaliar(dict_respostas):
+    if avaliar(traduzido_value):
         st.error('Usuário sem potencial')
     else:
         st.success('Usuário potencial')
-
-# PESSOAL
-# genero (gender) radio
-# Idoso (seniorCitizen) radio
-# casado (Partner) radio
-# dependentes (Dependents) radio
-
-# ASSINATURA
-# PhoneService selectbox
-# MultipleLines selectbox
-# InternetService selectbox
-# OnlineSecurity selectbox
-# OnlineBackup selectbox
-# DeviceProtection selectbox
-# TechSuport selectbox
-# StreamingTV selectbox
-# StreamingMovies selectbox
-
-# CONTRATO
-# tenure (meses contrato) Slider
-# Contract selectbox
-# PaperlessBilling radio
-# PaymentMethod selectbox
-# Charges.Month Slider
-# Charges.Total Slider
